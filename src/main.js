f847ca43516bc0110f0e5fc9dbf91d94d1089f55
hikaruna <me@hikaruna.net>
2016-05-08 11:15:57 +0900

    くまのサンプルが動くところまで

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-require('enchant.js');
-enchant();
+window.onload = function(){
+  require('enchant.js');
+  enchant();
+  var game = new Core(320, 320);
+  game.preload("assets/images/chara1.png");
 
-window.onload = function() {
-  var game = new Core();
-  game.onload = function () {
-    game.currentScene.backgroundColor = 'red';
-  };
-  game.start();
+  game.on('load', function(){
+    var Bear = require('bear');
+    var bear = new Bear();
+
+    game.rootScene.on('touchstart', function(e) {
+      bear.walkTo(e.x, e.y);
+    });
+
+    game.rootScene.addChild(bear);
+  });
+
+  game.debug();
 };
